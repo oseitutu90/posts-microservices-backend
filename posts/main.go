7d52206ed56b9902a5c0e84e9efcde6b9a9b0536
oseitutu90 <otamoabin2@icloud.com>
2022-12-19 04:04:15 -0600

    bring in comments api integration

--- posts/main.go
+++ posts/main.go
@@ -3,8 +3,9 @@
 package main
 
 import (
-	"bytes"
-	"log"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -13,18 +14,19 @@ import (
 )
 
 type Post struct {
-	Id          uint   `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Id          uint      `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Comments    []Comment `json:"comments" gorm-" default:[]"` // create a slice of comments
+}
+
+type Comment struct { // Comment model
+	Id     uint   `json:"id"`
+	PostId uint   `json: "postId"`
+	Text   string `json: "text"`
 }
 
 func main() {
-	var (
-		buf    bytes.Buffer
-		logger = log.New(&buf, "logger: ", log.Lshortfile)
-	)
-
-	// https://github.com/go-sql-driver/mysql
 	dsn := "root:Geforce229!@tcp(localhost:3306)/posts_ms?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) // Pass the connection to the database to the gorm.Open function
 
@@ -32,7 +34,8 @@ func main() {
 		panic(err)
 	}
 
-	logger.Println("Connected to the database")
+	db.AutoMigrate(&Comment{}) // create a table for the comment model
+
 	db.AutoMigrate(&Post{})
 
 	app := fiber.New() // create a new fiber app
@@ -43,6 +46,21 @@ func main() {
 		var posts []Post // create a slice of posts
 
 		db.Find(&posts) // find all posts
+
+		for i, post := range posts {
+			//http.Get("http://localhost:8000/api/comments/" + post.Id) // get all comments for the post
+
+			response, err := http.Get(fmt.Sprintf("http://localhost:8001/api/posts/%d/comments", post.Id))
+			// get all comments for the post
+			// there are better ways of displaying the comments
+
+			if err != nil {
+				return err
+			}
+			var comments []Comment
+			json.NewDecoder(response.Body).Decode(&comments) // decode the response body
+			posts[i].Comments = comments                     // assign the comments to the post
+		}
 
 		return c.JSON(posts) // return all posts
 	})
